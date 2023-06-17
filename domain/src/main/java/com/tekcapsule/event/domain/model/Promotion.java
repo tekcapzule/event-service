a00cc8dfd48157062fb153a8736fc44606e9519c
Linjith <linjith.kunnon@gmail.com>
2023-06-17 16:53:47 +0530

    git added deynamo annotation

--- domain/src/main/java/com/tekcapsule/event/domain/model/Promotion.java
+++ domain/src/main/java/com/tekcapsule/event/domain/model/Promotion.java
@@ -1,5 +1,6 @@
 package com.tekcapsule.event.domain.model;
 
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBDocument;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -11,6 +12,7 @@ import lombok.NoArgsConstructor;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@DynamoDBDocument
 public class Promotion {
     private Boolean promoted;
     private String endsOnUtc;
